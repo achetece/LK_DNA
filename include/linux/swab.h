89f344992f88aae27f8183775a7950bc66f82695
Lonnie Kerchief <aostang@gmail.com>
2013-01-15 18:28:03 -0500

    ignore -Wmaybe-uninitialized compile errors

--- include/linux/swab.h
+++ include/linux/swab.h
@@ -5,6 +5,8 @@
 #include <linux/compiler.h>
 #include <asm/swab.h>
 
+#pragma GCC diagnostic ignored "-Wmaybe-uninitialized"
+
 /*
  * casts are necessary for constants, because we never know how for sure
  * how U/UL/ULL map to __u16, __u32, __u64. At least not in a portable way.
